8d1b4d1f4a9351858676a6cb225a0d72efeb3220
Alex <alex@coliper.org>
2018-10-14 22:38:22 +0200

    extract common test classes for proxy and codegen

---
+++ src/test/java/org/coliper/ibean/factory/AbstractBeanEqualsTest.java
@@ -0,0 +1,241 @@
+/**
+ * 
+ */
+package org.coliper.ibean.factory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.coliper.ibean.BeanTypeWithCustomEquals;
+import org.coliper.ibean.EmptyBean;
+import org.coliper.ibean.EmptyBeanImpl;
+import org.coliper.ibean.IBean;
+import org.coliper.ibean.IBeanFactory;
+import org.coliper.ibean.PrimitivesBeanClassic;
+import org.coliper.ibean.PrimitivesBeanClassicImpl;
+import org.coliper.ibean.SampleBeanClassic;
+import org.coliper.ibean.SampleBeanClassicImpl;
+import org.coliper.ibean.SampleBeanModern;
+import org.coliper.ibean.SampleBeanModernImpl;
+import org.junit.Test;
+
+public abstract class AbstractBeanEqualsTest {
+    protected IBeanFactory factory;
+
+    protected AbstractBeanEqualsTest(IBeanFactory factory) {
+        this.factory = factory;
+    }
+
+    protected abstract void switchToModernStyleBuilder();
+
+    @Test
+    public void testEmptyBean() {
+        EmptyBeanImpl regularBean = new EmptyBeanImpl();
+        EmptyBean bean1 = this.factory.create(EmptyBean.class);
+        EmptyBean bean2 = this.factory.create(EmptyBean.class);
+        assertTrue(bean1.equals(bean2));
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+    }
+
+    @Test
+    public void testSampleBeanClassic() throws Exception {
+        SampleBeanClassicImpl regularBean = new SampleBeanClassicImpl();
+        SampleBeanClassic bean1 = this.factory.create(SampleBeanClassic.class);
+        SampleBeanClassic bean2 = this.factory.create(SampleBeanClassic.class);
+
+        assertTrue(bean1.equals(bean2)); // initially both beans are empty:
+                                         // beans equal
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.fillWithTestValues();
+
+        regularBean.copyTo(bean1); // only bean1 has new values: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.copyTo(bean2); // bean2 now also has new values: beans equal
+        assertTrue(bean1.equals(bean2));
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        bean1.setString("other"); // change bean1 again: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+    }
+
+    @Test
+    public void testSampleBeanModern() throws Exception {
+        this.switchToModernStyleBuilder();
+        SampleBeanModernImpl regularBean = new SampleBeanModernImpl();
+        SampleBeanModern bean1 = this.factory.create(SampleBeanModern.class);
+        SampleBeanModern bean2 = this.factory.create(SampleBeanModern.class);
+
+        assertTrue(bean1.equals(bean2)); // initially both beans are empty:
+                                         // beans equal
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.fillWithTestValues();
+
+        regularBean.copyTo(bean1); // only bean1 has new values: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.copyTo(bean2); // bean2 now also has new values: beans equal
+        assertTrue(bean1.equals(bean2));
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        bean1.string("other"); // change bean1 again: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+    }
+
+    @Test
+    public void testPrimitivesBeanClassicWithValues() throws Exception {
+        PrimitivesBeanClassicImpl regularBean = new PrimitivesBeanClassicImpl();
+        PrimitivesBeanClassic bean1 = this.factory.create(PrimitivesBeanClassic.class);
+        PrimitivesBeanClassic bean2 = this.factory.create(PrimitivesBeanClassic.class);
+
+        assertTrue(bean1.equals(bean2)); // initially both beans are empty:
+                                         // beans equal
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.fillWithTestValues();
+
+        regularBean.copyTo(bean1); // only bean1 has new values: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        regularBean.copyTo(bean2); // bean2 now also has new values: beans equal
+        assertTrue(bean1.equals(bean2));
+        assertTrue(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+
+        bean1.setIntPrimitive(666); // change bean1 again: beans differ
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertFalse(bean1.equals(regularBean));
+    }
+
+    public static interface Base {
+        int getY();
+
+        void setY(int i);
+    }
+
+    public static interface A extends Base {
+        int getX();
+
+        void setX(int i);
+    }
+
+    public static interface B extends Base {
+        int getX();
+
+        void setX(int i);
+    }
+
+    @Test
+    public void testDifferentTypes() {
+        A beanA = this.factory.create(A.class);
+        B beanB = this.factory.create(B.class);
+        assertFalse(beanA.equals(beanB));
+        assertFalse(beanB.equals(beanA));
+
+        beanA.setX(13);
+        beanB.setX(13);
+        assertFalse(beanA.equals(beanB));
+        assertFalse(beanB.equals(beanA));
+    }
+
+    @Test
+    public void testBaseTypes() {
+        A beanA = this.factory.create(A.class);
+        Base beanB = this.factory.create(Base.class);
+        assertFalse(beanA.equals(beanB));
+        assertFalse(beanB.equals(beanA));
+
+        beanA.setY(13);
+        beanB.setY(13);
+        assertFalse(beanA.equals(beanB));
+        assertFalse(beanB.equals(beanA));
+    }
+
+    @Test
+    public void testCycle() throws Exception {
+        SampleBeanClassic bean1 = this.factory.create(SampleBeanClassic.class);
+        SampleBeanClassic bean2 = this.factory.create(SampleBeanClassic.class);
+        SampleBeanClassic bean3 = this.factory.create(SampleBeanClassic.class);
+
+        // one bean cycle (1) <-> (1) , (2) <-> (2)
+        bean1.setSelf(bean1);
+        bean2.setSelf(bean2);
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertTrue(bean1.equals(bean1));
+        assertTrue(bean2.equals(bean2));
+
+        // two bean cycle (1) <-> (2)
+        bean2.setSelf(bean1);
+        bean1.setSelf(bean2);
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertTrue(bean1.equals(bean1));
+        assertTrue(bean2.equals(bean2));
+
+        // three bean cycle (1) -> (2) -> (3) -> (1)
+        bean1.setSelf(bean2);
+        bean2.setSelf(bean3);
+        bean3.setSelf(bean1);
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertTrue(bean1.equals(bean1));
+        assertTrue(bean2.equals(bean2));
+
+        // indirect cycle (1) -> (2) <-> (3)
+        bean1.setSelf(bean2);
+        bean2.setSelf(bean3);
+        bean3.setSelf(bean2);
+        assertFalse(bean1.equals(bean2));
+        assertFalse(bean2.equals(bean1));
+        assertTrue(bean1.equals(bean1));
+        assertTrue(bean2.equals(bean2));
+
+        // symmetric cycle (1) -> (3) <-> (3) <- (2)
+        bean1.setSelf(bean3);
+        bean2.setSelf(bean3);
+        bean3.setSelf(bean3);
+        assertTrue(bean1.equals(bean2));
+        assertTrue(bean2.equals(bean1));
+        assertTrue(bean1.equals(bean1));
+        assertTrue(bean2.equals(bean2));
+
+    }
+
+    @Test
+    public void testCustomEquals() {
+        BeanTypeWithCustomEquals bean1 = IBean.newOf(BeanTypeWithCustomEquals.class);
+        bean1.setId("#1");
+        bean1.setName("John");
+
+        BeanTypeWithCustomEquals bean2 = IBean.newOf(BeanTypeWithCustomEquals.class);
+        bean2.setId("#2");
+        bean2.setName("John");
+
+        assertThat(bean1.equals(bean2)).isFalse();
+
+        bean2.setId("#1");
+        bean2.setName("Mary");
+        assertThat(bean1.equals(bean2)).isTrue();
+    }
+}
---
+++ src/test/java/org/coliper/ibean/factory/extension/AbstractNullSafeTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.coliper.ibean.factory.extension;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.coliper.ibean.extension.NullSafe;
+import org.coliper.ibean.extension.NullSafetyException;
+import org.coliper.ibean.proxy.ProxyIBeanFactory;
+import org.junit.Test;
+
+/**
+ * @author alex@coliper.org
+ *
+ */
+public abstract class AbstractNullSafeTest {
+
+    public static interface NullSafeBean extends NullSafe {
+      //@formatter:off
+        String getString();
+        void setString(String s);
+
+        int getInt();
+        void setInt(int i);
+      //@formatter:on
+    }
+
+    protected abstract ProxyIBeanFactory createBeanFactory();
+
+    @Test()
+    public void testNullSafetyExceptionAfterCreation() throws Exception {
+        NullSafeBean bean = this.createBeanFactory().create(NullSafeBean.class);
+        bean.getInt();
+        assertThatExceptionOfType(NullSafetyException.class).isThrownBy(() -> bean.getString());
+    }
+
+    @Test()
+    public void testNullSafetyExceptionFieldSetBackToNull() throws Exception {
+        NullSafeBean bean = this.createBeanFactory().create(NullSafeBean.class);
+        bean.getInt();
+        bean.setString("3242342");
+        bean.getString();
+        bean.setString(null);
+        assertThatExceptionOfType(NullSafetyException.class).isThrownBy(() -> bean.getString());
+    }
+}
